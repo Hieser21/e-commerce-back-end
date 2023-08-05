34543a773546d9e990827f31e24abe944795ee28
Arafat Rahaman <96245505+Hieser21@users.noreply.github.com>
2023-08-05 15:01:03 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ import products from './routes/products.js'
 import orders from './routes/orders.js'
 import contacts from './routes/contacts.js'
 const app = express()
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const watcher = chokidar.watch('uploads', {
+    persistent: true,
+    awaitWriteFinish: true,
+    cwd: __dirname
+})
 
 const connectDB = async () => {
   try {
@@ -26,7 +35,10 @@ app.use('/users', users)
 app.use('/products', products)
 app.use('/orders', orders)
 app.use('/contacts', contacts)
-
+watcher.on('add', path => {console.log(path);
+                      provider.upload(path, path)})
+  watcher.on('unlink', path => { console.log(path);
+                         provider.delete(path)})
 
 connectDB().then(() => {
     app.listen(9000, () => {
